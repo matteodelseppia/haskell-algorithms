14d8ee9147582bb90ea4cf1428281e38560702a7
Matteo Del Seppia <71389092+matteodelseppia@users.noreply.github.com>
2023-09-15 11:05:10 +0200

    Update MaxHeap.hs

--- maxheap/MaxHeap.hs
+++ maxheap/MaxHeap.hs
@@ -8,27 +8,27 @@ data MaxHeap nodeType = MaxHeap {
   len :: Int
 } deriving (Show)
 
-buildMaxHeap :: (Num nodeType, Ord nodeType) => [nodeType] -> MaxHeap nodeType
+buildMaxHeap :: (Ord nodeType) => [nodeType] -> MaxHeap nodeType
 buildMaxHeap [] = MaxHeap {array = [], len = 0}
 buildMaxHeap arr = MaxHeap {array = (reverse.sort)arr, len = (length arr)}
 
-heapMax :: (Num nodeType, Ord nodeType) => MaxHeap nodeType -> Maybe nodeType
+heapMax :: (Ord nodeType) => MaxHeap nodeType -> Maybe nodeType
 heapMax heap
     | null (array heap) = Nothing
     | otherwise = Just ((head.array) heap)
 
-slice :: (Num a) => [a] -> Int -> Int -> [a]
+slice :: (Ord a) => [a] -> Int -> Int -> [a]
 slice array from to = case from >= to of
     True -> []
     False -> take (to - from) (drop from array)
 
-swap :: (Num nodeType, Ord nodeType) => [nodeType] -> Int -> Int -> Int -> [nodeType]
+swap :: (Ord nodeType) => [nodeType] -> Int -> Int -> Int -> [nodeType]
 swap array first second size = do 
     let firstValue = array !! first
     let secondValue = array !! second
     ((take first array) ++ [secondValue] ++ (slice array (first+1) second) ++ [firstValue] ++ (slice array (second+1) size))
 
-heapifyAfterInsert :: (Num nodeType, Ord nodeType) => [nodeType] -> Int -> Int -> [nodeType]
+heapifyAfterInsert :: (Ord nodeType) => [nodeType] -> Int -> Int -> [nodeType]
 heapifyAfterInsert heap 0 _ = heap
 heapifyAfterInsert heap pos size
     | fatherValue >= value = heap
@@ -40,18 +40,18 @@ heapifyAfterInsert heap pos size
         fatherPos = (pos-1) `div` 2
         fatherValue = heap !! fatherPos
 
-insertOne :: (Num nodeType, Ord nodeType) => MaxHeap nodeType -> nodeType -> MaxHeap nodeType
+insertOne :: (Ord nodeType) => MaxHeap nodeType -> nodeType -> MaxHeap nodeType
 insertOne heap element = do
     let arr = array heap
     let size = len heap
     let newArr = heapifyAfterInsert (arr ++ [element]) size (size+1)
     heap { array = newArr, len = size + 1}
 
-insertList :: (Num nodeType, Ord nodeType) => MaxHeap nodeType -> [nodeType] -> MaxHeap nodeType
+insertList :: (Ord nodeType) => MaxHeap nodeType -> [nodeType] -> MaxHeap nodeType
 insertList heap [] = heap
 insertList heap (h:t) = insertList (insertOne heap h) t
 
-pop :: (Num nodeType, Ord nodeType) => MaxHeap nodeType -> (MaxHeap nodeType, Maybe nodeType)
+pop :: (Ord nodeType) => MaxHeap nodeType -> (MaxHeap nodeType, Maybe nodeType)
 pop heap 
     | null (array heap) = (heap, Nothing)
     | otherwise = do
@@ -64,13 +64,13 @@ pop heap
             (heap { array = adjusted, len = (oldLen - 1)}, out)
 
 
-heapifyAfterPop :: (Num nodeType, Ord nodeType) => [nodeType] -> Int -> Int -> [nodeType]
+heapifyAfterPop :: (Ord nodeType) => [nodeType] -> Int -> Int -> [nodeType]
 heapifyAfterPop array pos 0 = []
 heapifyAfterPop array pos size
     | leftSonIndex >= size = trace "leftSonIndex >= size" array
     | leftSonIndex < size || rightSonIndex < size = do
         let leftSonVal = array !! leftSonIndex
-        let rightSonVal =  if rightSonIndex < size then (array !! rightSonIndex) else (val - 1)
+        let rightSonVal =  if rightSonIndex < size then (array !! rightSonIndex) else val
         if leftSonVal > val || rightSonVal > val then do
             let (toSwapIndex, toSwapVal) = if leftSonVal > rightSonVal then (leftSonIndex, (array !! leftSonIndex))  else (rightSonIndex, (array !! rightSonIndex))
             let newHeap = (swap array pos toSwapIndex size)
