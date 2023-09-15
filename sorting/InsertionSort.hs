e5a6ed896c2bfe05f027c93771193708b72a707e
Matteo Del Seppia <matteodelseppia.cc@gmail.com>
2023-09-16 00:47:42 +0200

    _

--- sorting/InsertionSort.hs
+++ sorting/InsertionSort.hs
@@ -1,9 +1,8 @@
 module InsertionSort where
 
 import Data.List
-import Debug.Trace
 
-adjust :: (Ord a, Show a) => [a] -> Int -> [a]
+adjust :: (Ord a) => [a] -> Int -> [a]
 adjust [] _ = []
 adjust [x] _ = [x]
 adjust list 0 = list
@@ -15,7 +14,7 @@ adjust list pos
         previous = list !! (pos-1)
         len = length list
 
-insertionSort' :: (Ord a, Show a) => [a] -> Int -> [a]
+insertionSort' :: (Ord a) => [a] -> Int -> [a]
 insertionSort' [] _ = []
 insertionSort' [x] _ = [x]
 insertionSort' list pos
@@ -27,5 +26,5 @@ insertionSort' list pos
         previous = list !! (pos-1)
         len = length list
 
-insertionSort :: (Ord a, Show a) => [a] -> [a]
+insertionSort :: (Ord a) => [a] -> [a]
 insertionSort list = insertionSort' list 1
