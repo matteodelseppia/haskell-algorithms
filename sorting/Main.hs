a73c03bd5093628a895c26df162f12c8e83b2db9
Matteo Del Seppia <71389092+matteodelseppia@users.noreply.github.com>
2023-09-15 18:58:00 +0200

    Update Main.hs

--- sorting/Main.hs
+++ sorting/Main.hs
@@ -6,5 +6,5 @@ import MergeSort
 main = do 
     list <- randomList 15 (-100) 100
     putStrLn $ "List to order: " ++ show list
-    putStrLn $ "Insertion sort: " ++ show (quickSort list)
+    putStrLn $ "Quick sort: " ++ show (quickSort list)
     putStrLn $ "Merge sort: " ++ show (mergeSort list)
