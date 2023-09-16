e58aa684898534324f41b8a0a18c5c06562f96a2
Matteo Del Seppia <matteodelseppia.cc@gmail.com>
2023-09-16 12:33:37 +0200

    _

--- sorting/QuickSort.hs
+++ sorting/QuickSort.hs
@@ -1,5 +1,9 @@
 module QuickSort where
 
+
 quickSort :: (Ord a) => [a] -> [a]
 quickSort [] = []
-quickSort (head' : tail') = quickSort [g | g <- tail', g <= head'] ++ [head'] ++ quickSort [g | g <- tail', g > head']
+--version with monads:
+quickSort (h:t) = (quickSort (t >>= \x -> if x <= h then [x] else [])) ++ [h] ++ (quickSort (t >>= \x -> if x > h then [x] else []))
+--version with list comprehension:
+--quickSort (head' : tail') = quickSort [g | g <- tail', g <= head'] ++ [head'] ++ quickSort [g | g <- tail', g > head']
